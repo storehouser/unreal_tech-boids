41b43122bf28cb310a51566ddbdbcab61afa670a
한상문 [Auggie] <auggie@nirvanana.net>
2026-04-30 10:45:18 +0900

    Boi기초적인 Boid 알고리즘ì의 계산 값 함수화 및 위치 갱신, BoidData를 통한 ì 위치, 속도, 회전 값등을 결정

--- Source/Beli/BeliSwarm.cpp
+++ Source/Beli/BeliSwarm.cpp
@@ -22,17 +22,51 @@ void ABeliSwarm::BeginPlay()
 	
 	check(IsValid(InstancedMeshComp));
 	
+	Boids.Reserve(MaxNumBoids);
+	
 	for (int32 i = 0; i < MaxNumBoids; ++i)
 	{
-		FTransform Dummy = FTransform::Identity;
-		Dummy.SetTranslation(FVector(FMath::RandRange(-100, 100), FMath::RandRange(-100, 100), FMath::RandRange(-100, 100)));
+		FBoidData& Boid = Boids[i];
+		Boid.Location = FVector(FMath::RandRange(-100, 100), FMath::RandRange(-100, 100), FMath::RandRange(-100, 100));
+		Boid.Velocity = FVector(FMath::RandRange(-100, 100), FMath::RandRange(-100, 100), FMath::RandRange(-100, 100));
+		Boid.Rotation = Boid.Velocity.ToOrientationRotator();
 		
-		InstancedMeshComp->AddInstance(Dummy);
+		InstancedMeshComp->AddInstance(Boid.GetTransaform());
 	}
 }
 
 void ABeliSwarm::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	check(IsValid(InstancedMeshComp));
+	
+	for (int32 i = 0; i < MaxNumBoids; ++i)
+	{
+		FBoidData& Boid = Boids[i];
+		
+		const FVector& Seperation = CalcSeperation(Boid);
+		const FVector& Alignment = CalcAlignment(Boid);
+		const FVector& Cohesion = CalcCohesion(Boid);
+		
+		Boid.Velocity += (Seperation + Alignment + Cohesion) * DeltaTime;
+		Boid.Rotation = FMath::RInterpTo(Boid.Rotation, Boid.Velocity.ToOrientationRotator(), DeltaTime, 0.1f);
+		Boid.Location += Boid.Velocity;
+	}
+}
+
+const FVector& ABeliSwarm::CalcSeperation(const FBoidData& InBoidData) const
+{
+	return FVector::ZeroVector;
+}
+
+const FVector& ABeliSwarm::CalcAlignment(const FBoidData& InBoidData) const
+{
+	return FVector::ZeroVector;
+}
+
+const FVector& ABeliSwarm::CalcCohesion(const FBoidData& InBoidData) const
+{
+	return FVector::ZeroVector;
 }
 
