5d9e6dfa6109e0f869ce89581ada76b834dddac8
Cookie <storehouser@gmail.com>
2026-05-06 14:16:40 +0900

    공간 그리드 해쉬를 위한 자료형 작성

---
+++ Source/Beli/Library/SpatialGridHashHelper.cpp
@@ -0,0 +1,31 @@
+
+#include "SpatialGridHashHelper.h"
+
+
+FSpatialGridHashHelper::FSpatialGridHashHelper(float InCellSize, int32 TotalNums)
+{
+	CellSize = InCellSize;
+	
+	// 해쉬 충돌을 피하기 위해 실제 데이터 수보다 크게 잡는다.
+	HashTableSize = TotalNums * 2;
+}
+
+FSpatialGrid FSpatialGridHashHelper::GetGridIndex(const FVector& Location) const
+{
+	return FSpatialGrid(FMath::FloorToInt(Location.X / CellSize), FMath::FloorToInt(Location.Y / CellSize), FMath::FloorToInt(Location.Z / CellSize));
+}
+
+int32 FSpatialGridHashHelper::GetHashKey(const FSpatialGrid& Grid) const
+{
+	constexpr int32 Prime1 = 73856093;
+	constexpr int32 Prime2 = 19349663;
+	constexpr int32 Prime3 = 83492791;
+	
+	const int32 Hash = (Grid.X * Prime1) ^ (Grid.Y & Prime2) ^ (Grid.Z * Prime3);
+	return FMath::Abs(Hash) % HashTableSize;
+}
+
+int32 FSpatialGridHashHelper::GetHashKeyFromLocation(const FVector& Location) const
+{
+	return GetHashKey(GetGridIndex(Location));
+}
---
+++ Source/Beli/Library/SpatialGridHashHelper.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+/** 공간 상의 위치 값과 공간에 나뉘어진 그리드의 기반 위치 값에 혼용을 줄이기 위해 별칭 선언. */
+using FSpatialGrid = FIntVector;
+
+
+/**
+ * Cell의 크기와 Hash이 필요한 Object들의 갯수를 미리 지정해 놓고 필요할 때 변환을 처리.
+ */
+struct FSpatialGridHashHelper
+{
+public:
+	FSpatialGridHashHelper(float InCellSize, int32 TotalNums);
+	
+	FSpatialGrid GetGridIndex(const FVector& Location) const;
+	int32 GetHashKey(const FSpatialGrid& Grid) const;
+	int32 GetHashKeyFromLocation(const FVector& Location) const;
+	
+private:
+	float CellSize;
+	int32 HashTableSize;	
+};
